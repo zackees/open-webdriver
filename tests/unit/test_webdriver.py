11f86e56c17b24b3a16dedb8e84bbd6b0b419a42
zackees <z@zackees.com>
2022-05-14 13:55:10 -0700

    nit comments

--- tests/unit/test_webdriver.py
+++ tests/unit/test_webdriver.py
@@ -2,11 +2,12 @@
     Tests the open_webdriver package.
 """
 
+import os
 import unittest
 
 from open_webdriver import open_webdriver
 
-FULL_TESTS = False
+FULL_TESTS = os.environ.get("FULL_TESTS", "0") == "0"
 
 if FULL_TESTS:
     all_drivers = ["chrome", "firefox", "brave"]
@@ -14,26 +15,26 @@ else:
     all_drivers = ["chrome"]
 
 
-def do_test(driver_name: str, headless: bool) -> bool:
+def do_google_test(driver_name: str, headless: bool) -> bool:
     """Runs the tests for a given driver."""
     with open_webdriver(driver_name=driver_name, headless=headless, verbose=True) as driver:
         driver.get("https://www.google.com")
         return driver.title == "Google"
 
 
-class GabDriverTest(unittest.TestCase):
-    """Gab driver test framework."""
+class OpenWebDriverTests(unittest.TestCase):
+    """Tester for open_webdriver.py"""
 
-    def test_gab_test(self) -> None:
-        """Tests that gab_test works."""
+    def test_google(self) -> None:
+        """Tests that google test works."""
         for driver in all_drivers:
-            ok = do_test(driver, headless=False)  # pylint: disable=invalid-name
+            ok = do_google_test(driver, headless=False)  # pylint: disable=invalid-name
             self.assertTrue(ok)
 
-    def test_gab_test_headless(self) -> None:
-        """Tests that gab_test works."""
+    def test_google_headless(self) -> None:
+        """Tests that google headless works."""
         for driver in all_drivers:
-            ok = do_test(driver, headless=True)  # pylint: disable=invalid-name
+            ok = do_google_test(driver, headless=True)  # pylint: disable=invalid-name
             self.assertTrue(ok)
 
 
