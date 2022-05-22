4c40c8dfc41d601dfbc663fbe493029903a2ee06
zackees <z@zackees.com>
2022-05-21 19:27:33 -0700

    adds os_env test

---
+++ open_webdriver/tests/unit/test_osenv.py
@@ -0,0 +1,21 @@
+"""
+    Tests the open_webdriver package.
+"""
+
+import os
+import sys
+import unittest
+from pprint import pprint
+
+
+class OpenWebDriverTests(unittest.TestCase):
+    """Tester for open_webdriver.py"""
+
+    @unittest.skipIf(sys.platform != "win32", "Windows only tests")
+    def test_osenv(self) -> None:
+        """Asserts that the win32 bug is fixed."""
+        pprint(dict(os.environ))
+
+
+if __name__ == "__main__":
+    unittest.main()
