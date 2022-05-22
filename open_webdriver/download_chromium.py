464ac542d79f5210905408a14621d93ef319d8c9
zackees <z@zackees.com>
2022-05-22 15:10:00 -0700

    adds permissions modification

--- open_webdriver/download_chromium.py
+++ open_webdriver/download_chromium.py
@@ -13,6 +13,14 @@ WDM_DIR = os.path.join(ROOT, ".wdm")
 WDM_CHROMIUM_DIR = os.path.join(WDM_DIR, "chromium")
 
 
+def _set_exe_permissions(start_dir: str) -> None:
+    """Sets the permissions on the chromium executable."""
+    for root, _, files in os.walk(start_dir):
+        for file_name in files:
+            path = os.path.join(root, file_name)
+            os.chmod(path, 0o755)
+
+
 def get_chromium_exe() -> str:
     """Fetches the chromium executable."""
     url_src = f"https://github.com/zackees/open-webdriver/raw/main/chromium/{sys.platform}.zip"
@@ -23,6 +31,7 @@ def get_chromium_exe() -> str:
         download(url=url_src, path=zip_dst, kind="file", progressbar=True, replace=False)
         assert os.path.exists(zip_dst), f"{zip_dst} does not exist."
         shutil.unpack_archive(zip_dst, WDM_CHROMIUM_DIR)
+        _set_exe_permissions(platform_dir)
         # Touch file.
         with open(finished_stamp, encoding="utf-8", mode="w") as filed:
             filed.write("")
