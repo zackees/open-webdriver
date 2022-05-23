75dcea277b41e2e8734eaa5bae6976341afe5bfc
zackees <z@zackees.com>
2022-05-22 20:03:13 -0700

    root is now sys.argv[0]

--- open_webdriver/path.py
+++ open_webdriver/path.py
@@ -3,10 +3,9 @@ Paths for the project.
 """
 
 import os
+import sys
 
-ROOT = os.path.dirname(os.path.dirname(__file__))
-os.chdir(ROOT)
-ROOT = os.path.relpath(ROOT, ".")
+ROOT = os.path.dirname(sys.argv[0])
 WDM_DIR = os.path.join(ROOT, ".wdm")
 WDM_CHROMIUM_DIR = os.path.join(WDM_DIR, "chromium")
 LOG_FILE = os.path.join(ROOT, "open_webdriver.log")
