8cf0e60a3d5442a1275c96c526cae2b7735e4c2f
zackees <z@zackees.com>
2025-03-15 17:42:46 -0700

    update

--- install.sh
+++
@@ -1,6 +0,0 @@
-python make_venv.py
-source activate.sh
-python -m pip install -U pip
-python -m pip install -r requirements.txt
-python -m pip install -r requirements.testing.txt
-python -m pip install -e .
